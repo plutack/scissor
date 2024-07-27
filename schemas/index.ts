571b66dce8c3fa35190c51c7cbc8d2741479e9e7
Talut Salako <salakotalut@gmail.com>
2024-07-27 23:09:24 +0100

    add schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,9 @@
 import * as z from "zod";
 
+const urlRegex =
+  /^((?:ftp|http|https)?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i;
+
+
 export const LoginSchema = z.object({
   email: z.string().email({ message: "Email is required" }),
   password: z.string().min(1, { message: "Password is required" }),
@@ -9,19 +13,23 @@ export const RegisterSchema = z
   .object({
     name: z.string().min(1, { message: "First name is required" }),
     email: z.string().email({ message: "Email is required" }),
-    password: z.string().min(1, { message: "Password is required" }),
+    password: z.string().min(1, { message: "Password is required" }), // TODO: Enforce strong password
     confirmPassword: z.string().min(1, { message: "Password is required" }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
     path: ["confirmPassword"],
   });
-const urlRegex =
-  /^((?:ftp|http|https)?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i;
+
+
 export const shortenLinkSchema = z.object({
   link: z.string().regex(urlRegex, { message: "Invalid URL" }),
   customSuffix: z
     .string()
     .min(3, { message: "Custom suffix is too short" })
     .optional(),
+});
+
+export const changeCustomSuffixSchema = z.object({
+  customSuffix: z.string().min(3, { message: "Custom suffix is too short" }),
 });
