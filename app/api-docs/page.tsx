5c564fe3f62c80d25e83ea4310656e68f953f3d7
Talut Salako <salakotalut@gmail.com>
2024-07-28 08:02:41 +0100

    fix open api yaml file

--- app/api-docs/page.tsx
+++ app/api-docs/page.tsx
@@ -1,16 +1,17 @@
 "use client"
-
 import React from 'react';
 
-import { API } from '@stoplight/elements';
-import '@stoplight/elements/styles.min.css';
+import '@stoplight/elements/styles.min.css';4
+
+import dynamic from "next/dynamic";
+const API = dynamic(() => import("@stoplight/elements").then((x) => x.API), { ssr: false });
 
 
 function ApiDocPage() {
   return (
     <div className="App">
       <API
-        apiDescriptionUrl="@/openapi.yaml"
+        apiDescriptionUrl="./openapi."
       />
     </div>
   );
