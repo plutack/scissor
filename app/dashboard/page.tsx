5837e085747a66d958f29db455e0d01a22acf832
Talut Salako <salakotalut@gmail.com>
2024-08-10 08:44:12 +0100

    render name

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -25,89 +25,80 @@ export default async function page() {
     <PageContainer scrollable={true}>
       <div className="space-y-2">
         <div className="flex h-full items-center justify-between space-y-2">
+           {/* TODO: add change display name? */}
           <h2 className="text-2xl font-bold tracking-tight">
-            Hi, Welcome back 👋
-          </h2>
+            Hi, {session?.user.name} 👋 
+          </h2> 
           {/* <div className="hidden items-center space-x-2 md:flex">
             <CalendarDateRangePicker />
             <Button>Download</Button>
           </div> */}
         </div>
-        <Tabs defaultValue="overview" className="space-y-4">
-          <TabsList>
-            <TabsTrigger value="overview">Overview</TabsTrigger>
-            <TabsTrigger value="analytics" disabled>
-              Analytics
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="overview" className="space-y-4">
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Total Clicks
-                  </CardTitle>
-                  <MousePointerClickIcon className="h-4 w-4 text-muted-foreground" />
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">
-                    {session?.user.totalClicks}
-                  </div>
-                  {/* <p className="text-xs text-muted-foreground">
+        <h3 className="text-xl font-bold tracking-tight space-y-4">Overview</h3>
+
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Clicks
+              </CardTitle>
+              <MousePointerClickIcon className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {session?.user.totalClicks}
+              </div>
+              {/* <p className="text-xs text-muted-foreground">
                   </p> */}
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Total Links Created
-                  </CardTitle>
-                  <Link2Icon className="h-4 w-4 text-muted-foreground" />
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">
-                    {session?.user.totalLinksCount}
-                  </div>
-                  {/* <p className="text-xs text-muted-foreground">
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Links Created
+              </CardTitle>
+              <Link2Icon className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {session?.user.totalLinksCount}
+              </div>
+              {/* <p className="text-xs text-muted-foreground">
                     +180.1% from last month
                   </p> */}
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Total Unique Countries
-                  </CardTitle>
-                  <FlagIcon className="h-4 w-4 text-muted-foreground" />
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">
-                    {session?.user.uniqueCountryCount}
-                  </div>
-                  {/* <p className="text-xs text-muted-foreground">
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Unique Countries
+              </CardTitle>
+              <FlagIcon className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {session?.user.uniqueCountryCount}
+              </div>
+              {/* <p className="text-xs text-muted-foreground">
                     +19% from last month
                   </p> */}
-                </CardContent>
-              </Card>
-            </div>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7">
-              <div className="col-span-4">
-                <DashboardTopCountriesChart />
-              </div>
-              <Card className="col-span-4 md:col-span-3">
-                <CardHeader>
-                  <CardTitle>Recent Links Created</CardTitle>
-                  <CardDescription>
-                    Here are your most recent links
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <RecentLinks />
-                </CardContent>
-              </Card>
-            </div>
-          </TabsContent>
-        </Tabs>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7">
+          <div className="col-span-4">
+            <DashboardTopCountriesChart />
+          </div>
+          <Card className="col-span-4 md:col-span-3">
+            <CardHeader>
+              <CardTitle>Recent Links Created</CardTitle>
+              <CardDescription>Here are your most recent links</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <RecentLinks />
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </PageContainer>
   );
