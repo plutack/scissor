5311c6d1c24d07441e424ca8512f8f4a6137d61a
Talut Salako <salakotalut@gmail.com>
2024-08-12 11:20:42 +0100

    Revert "rough test"
    This reverts commit e29fc3074dfda4ee65cdb3119aaafc343370a3d8.

--- pages/api/socketio.ts
+++ pages/api/socketio.ts
@@ -2,6 +2,8 @@ import { Server as ServerIO } from "socket.io";
 import * as userService from "../../services/user-service";
 import { NextApiResponseServerIo } from "@/types/socketio";
 import { NextApiRequest } from "next";
+import { Server as NetServer } from "http";
+import { getUserIdFromRequest } from "@/utils/auth";
 
 export const config = {
   api: {
@@ -20,7 +22,7 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
     if (!res.socket.server.io) {
       console.log("Initializing new Socket.io instance");
       const io = new ServerIO(res.socket.server as any, {
-        path: "/api/scketio",
+        path: "/api/socketio",
         addTrailingSlash: false,
         cors: {
           origin: process.env.NEXT_PUBLIC_BASE_URl,
