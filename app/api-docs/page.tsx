6fb4718edd0ecbb4c70876005c87a977af88c723
Talut Salako <salakotalut@gmail.com>
2024-08-12 14:46:33 +0100

    add api document

--- app/api-docs/page.tsx
+++ app/api-docs/page.tsx
@@ -1,19 +1,20 @@
-"use client"
-import React from 'react';
-import '@stoplight/elements/styles.min.css';
+"use client";
+import React from "react";
+import "@stoplight/elements/styles.min.css";
 import dynamic from "next/dynamic";
 import PageContainer from "@/components/layout/page-container";
-const API = dynamic(() => import("@stoplight/elements").then((x) => x.API), { ssr: false });
+const API = dynamic(() => import("@stoplight/elements").then((x) => x.API), {
+  ssr: false,
+});
 
 // link https://raw.githubusercontent.com/plutack/nextjsauth-template/main/openapi.yaml
 function ApiDocPage() {
   return (
-    <PageContainer scrollable><div className="App">
-    <API
-      apiDescriptionUrl="https://raw.githubusercontent.com/plutack/nextjsauth-template/main/openapi.yaml"
-    />
-  </div></PageContainer>
-    
+    <PageContainer scrollable>
+      <div className="App">
+        <API apiDescriptionUrl="https://raw.githubusercontent.com/plutack/nextjsauth-template/main/openapi.yaml" />
+      </div>
+    </PageContainer>
   );
 }
 
