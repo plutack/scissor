75a354e26f4bf943756ca66247c41c8164fcce50
Talut Salako <salakotalut@gmail.com>
2024-08-13 22:32:55 +0100

    create rate limit function

---
+++ utils/rate-limit.ts
@@ -0,0 +1,19 @@
+import ErrorWithStatus from "@/Exception/custom-error";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(20, "1 m"),
+});
+
+const rateLimitIP = async (request: Request) => {
+  const ip = request.headers.get("x-forwarded-for") ?? "";
+  const { success } = await ratelimit.limit(ip);
+  if (!success) {
+    throw new ErrorWithStatus("Too Many Requests", 429);
+  }
+  return;
+};
+
+export default rateLimitIP;
