963617d6c83303122fddc824c2beb18c30beeab4
Talut Salako <salakotalut@gmail.com>
2024-08-15 02:56:06 +0100

    minor changes

--- actions/login.ts
+++ actions/login.ts
@@ -10,7 +10,7 @@ export const login = async (
   values: z.infer<typeof loginSchema>,
 ): Promise<ErrorMessage> => {
   try {
-    const data = await ky
+    await ky
       .post("/api/auth/login", {
         json: values,
       })
