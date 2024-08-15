655d9c00b3bd13b33afb57d1ca6a3564d4534281
Talut Salako <salakotalut@gmail.com>
2024-08-15 03:14:01 +0100

    fix redirect

--- actions/logout.ts
+++ actions/logout.ts
@@ -2,7 +2,5 @@ import { signOut } from "next-auth/react";
 import { loginRoute } from "@/routes";
 
 export const logout = async () => {
-  await signOut({
-    callbackUrl: loginRoute,
-  });
+  await signOut();
 };
