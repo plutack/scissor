b8e1220647236948429d5595fecb84d6843e6dc4
Talut Salako <salakotalut@gmail.com>
2024-08-16 11:16:58 +0100

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,28 @@
-import Providers from '@/components/layout/providers';
-import { Toaster } from '@/components/ui/toaster';
-import '@uploadthing/react/styles.css';
-import type { Metadata } from 'next';
-import NextTopLoader from 'nextjs-toploader';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { auth } from '@/auth';
+import Providers from "@/components/layout/providers";
+import { Toaster } from "@/components/ui/toaster";
+import "@uploadthing/react/styles.css";
+import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { auth } from "@/auth";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Next Shadcn',
-  description: 'Basic dashboard with Next.js and Shadcn'
+  title: "Scissor - URL Shortener",
+  description: "Shorten, customize, and track your URLs with Scissor.",
 };
 
 export default async function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   const session = await auth();
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} overflow-hidden `}
-        
-      >
+      <body className={`${inter.className} overflow-hidden `}>
         <NextTopLoader showSpinner={false} />
         <Providers session={session}>
           <Toaster />
